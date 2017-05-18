3a64153e388ded64557af0c9825da61c4bb52f6d
Lisa Hua <lisahua46@gmail.com>
2017-05-18 10:53:38 -0500

    update

---
+++ eval-log/check_result.sh
@@ -0,0 +1,40 @@
+grep "AbstractCategoryItemRenderer.java-om-35-0" ./Chart_1/valid.txt
+grep "Week.java-am-14-1" ./Chart_8/valid.txt
+grep "TimeSeries.java-cm-2-0" ./Chart_9/valid.txt
+grep "ShapeUtilities.java-am-5-0" ./Chart_11/valid.txt
+grep "ValueMarker.java-mem-5-12" ./Chart_20/valid.txt
+grep "GrayPaintScale.java-am-1-0 " ./Chart_24/valid.txt
+
+grep "ControlFlowAnalysis.java-am-2-2 " ./Closure_14/valid.txt
+grep "LightweightMessageFormatter.java-om-3-2" ./Closure_62/valid.txt
+grep "MinimizeExitPoints.java-cm-2-1" ./Closure_126/valid.txt
+
+grep "CharSequenceTranslator.java-am-15-1 " ./Lang_6/valid.txt
+grep "StopWatch.java-ca-5-2" ./Lang_55/valid.txt
+grep "StrBuilder.java-am-0-1" ./Lang_59/valid.txt
+
+grep "Complex.java-am-0-0" ./Math_5/valid.txt
+grep "SimplexTableau.java-ov-23-0 " ./Math_33/valid.txt
+# grep "" ./Math_50/valid.txt
+grep "FastMath.java-am-0-2" ./Math_59/valid.txt
+grep "BisectionSolver.java-ov-0-1 " ./Math_70/valid.txt
+grep "SimplexSolver.java-om-24-1" ./Math_82/valid.txt
+grep "UnivariateRealSolverUtils.java-om-50-2" ./Math_85/valid.txt
+
+
+# AbstractCategoryItemRenderer.java-om-35-0 8.45511889458
+# Week.java-am-14-1 8.09847402573
+# TimeSeries.java-cm-2-0 8.76622486115
+# ShapeUtilities.java-am-5-0 8.39263892174
+# ValueMarker.java-mem-5-12 9.52980113029
+# GrayPaintScale.java-am-1-0 5.67739582062
+# ControlFlowAnalysis.java-am-2-2 19.9706850052
+# # Closure_62
+# MinimizeExitPoints.java-cm-2-1 17.3217320442
+# CharSequenceTranslator.java-am-15-1 1.13031196594
+# StopWatch.java-ca-5-2 671.461291075
+# # Lang_59
+# # Math_59
+# BisectionSolver.java-ov-0-1 2.1005179882
+# SimplexSolver.java-om-24-1 2.22020506859
+# UnivariateRealSolverUtils.java-om-50-2 4.50870800018
